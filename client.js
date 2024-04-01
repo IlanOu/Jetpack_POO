824e8cdb97046de6d7ebcd7a9816642399781cbc
Nay <ilan.outyey@gmail.com>
2024-04-01 11:49:35 +0200

    client socket

--- client.js
+++ client.js
@@ -1,9 +1,15 @@
+let connected = false
+
+
 function setupClient() {
+    console.log("En attente de connexion...")
+
     // Connexion au serveur WebSocket
-    socket = new WebSocket('ws://localhost:3000');
+    socket = new WebSocket('ws://192.168.40.62:80');
 
     // Événement déclenché lorsque la connexion est établie
     socket.onopen = function(event) {
+        connected = true;
         console.log('Connexion établie avec le serveur WebSocket');
     };
 
@@ -15,11 +21,13 @@ function setupClient() {
 
     // Événement déclenché en cas d'erreur de connexion
     socket.onerror = function(error) {
+        connected = false
         console.error('Erreur de connexion au serveur WebSocket:', error);
     };
 
     // Événement déclenché lorsque la connexion est fermée
     socket.onclose = function(event) {
+        connected = false
         console.log('Connexion WebSocket fermée');
     };
 }
--- sketch.js
+++ sketch.js
@@ -17,59 +17,68 @@ function setup() {
 
 function draw() {
   background(220);
- 
-  if (player.isAlive == true) {
 
+  if (connected){
 
-    if (!player.colliding.bottom && !keys.ArrowDown) {
-      player = applyGravity(player, gravity, friction);
+    if (player.isAlive == true) {
+  
+  
+      if (!player.colliding.bottom && !keys.ArrowDown) {
+        player = applyGravity(player, gravity, friction);
+      }
+  
+      // Collisions with map borders
+      playerCollideMap()
+  
+      // Player movements
+      moveObject(player)
+  
+      // Draw map borders
+      drawBorders()
+  
+      // Draw player
+      drawPlayer()
+  
+  
+      drawRocket(rocket_laser)
+      launchRocket(rocket_laser)
+      drawRocket(rocket)
+      launchRocket(rocket)
+  
+      // console.log(player)
+      // console.log(rocket_laser)
+      // console.log(rocket)
+  
+      player = processKillPlayer(player, rocket_laser)
+      player = processKillPlayer(player, rocket)
+  
+  
+      fill(0, 0, 0)
+      noStroke()
+      textSize(20)
+      text('Score : ' + points, 10, 20);
+  
+      // draw life
+      image(lifeImage[player.life], 300, 5, 100, 20)
+  
+    } else {
+      fill(0, 0, 0)
+      noStroke()
+      textSize(50)
+      text('💀 Vous êtes mort', 0, 175);
+      textSize(20)
+      text('Score : ' + points, 150, 225);
+  
+  
+      drawReplayButton(replayButton)
     }
-
-    // Collisions with map borders
-    playerCollideMap()
-
-    // Player movements
-    moveObject(player)
-
-    // Draw map borders
-    drawBorders()
-
-    // Draw player
-    drawPlayer()
-
-
-    drawRocket(rocket_laser)
-    launchRocket(rocket_laser)
-    drawRocket(rocket)
-    launchRocket(rocket)
-
-    // console.log(player)
-    // console.log(rocket_laser)
-    // console.log(rocket)
-
-    player = processKillPlayer(player, rocket_laser)
-    player = processKillPlayer(player, rocket)
-
-
+  }else{
     fill(0, 0, 0)
     noStroke()
-    textSize(20)
-    text('Score : ' + points, 10, 20);
-
-    // draw life
-    image(lifeImage[player.life], 300, 5, 100, 20)
-
-  } else {
-    fill(0, 0, 0)
-    noStroke()
-    textSize(50)
-    text('💀 Vous êtes mort', 0, 175);
-    textSize(20)
-    text('Score : ' + points, 150, 225);
-
-
-    drawReplayButton(replayButton)
+    textSize(30)
+    text('🛜 En attente de connexion', 20, 200);
   }
+ 
 
 
 
